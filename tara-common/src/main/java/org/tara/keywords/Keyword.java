af5b81983f115d3b45585fe6d9f247cf784f48b8
Joel Lacour <lacour.joel@web.de>
2017-10-28 14:04:55 +0200

    add tara-common

---
+++ tara-common/src/main/java/org/tara/keywords/Keyword.java
@@ -0,0 +1,13 @@
+package org.tara.keywords;
+
+public enum Keyword {
+
+	IF,
+	ELSE,
+	WHILE,
+	FOR,
+	
+	PACKAGE,
+	CLASS,
+	FUNCTION
+}
---
+++ tara-common/src/main/java/org/tara/types/Operator.java
@@ -0,0 +1,16 @@
+package org.tara.types;
+
+public enum Operator {
+
+	ASSIGN,
+	
+	EQUALS,
+	
+	ADD,
+	SUBTRACT,
+	MULTIPLY,
+	DIVIDE,
+	
+	INCREMENT,
+	DECREMENT
+}
